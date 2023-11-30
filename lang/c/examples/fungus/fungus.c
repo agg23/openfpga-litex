71560e35644e4cef19444b7ee6c1d14f6d30e0c9
mcc <andi.m.mcclure@gmail.com>
2023-11-29 20:12:43 -0500

    Diamond moving slowly downward (but has problems with memory segments)

--- lang/c/examples/fungus/fungus.c
+++ lang/c/examples/fungus/fungus.c
@@ -1,3 +1,7 @@
+// Fluidly expanding colors
+// Sample contributed by Andi McClure, available under Creative Commons Zero (public domain)
+// If you substantially reuse this code a credit would be appreciated but is not required
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -26,16 +30,21 @@
 // Given an axis of size n, what offset is needed to center the group of pillars?
 #define PILLARS_BASE(n) (((n)-PILLARS_SIZE)/2)
 
-#define CANDIDATES_COUNT 100
-#define WINNERS_COUNT 10
+#define CANDIDATE_COUNT 100
+#define WINNER_COUNT 100
 
 // What index within the framebuffer is this pixel at?
-#define at(x,y) (((y)*DISPLAY_WIDTH)+(x))
+#define AT(x,y) (((y)*DISPLAY_WIDTH)+(x))
 
 typedef struct {
 	uint16_t x;
 	uint16_t y;
 } Candidate;
+
+inline Candidate make_candidate(int x, int y) {
+	Candidate candidate = {x,y};
+	return candidate;
+}
 
 int main(void)
 {
@@ -57,16 +66,20 @@ int main(void)
 				int y_block = y_root+by*(PILLAR_SIZE+PILLAR_GAP), x_block = x_root+bx*(PILLAR_SIZE+PILLAR_GAP);
 				for(int y = 0; y < PILLAR_SIZE; y++) {
 					for(int x = 0; x < PILLAR_SIZE; x++) {
-						fb[at(x_block+x, y_block+y)] = COLOR(32, 0, 0);
+						fb[AT(x_block+x, y_block+y)] = COLOR(32, 0, 0);
 					}
 				}
 			}
 		}
 	}
 
-	Candidate candidates[100][2];
+	// Who needs a heap anyway
+	Candidate candidates[2][CANDIDATE_COUNT];
+	int candidates_len[2] = {0,0};
 	int current = 0;
-	int color = 0;
+	int color = COLOR(0,32,0);
+	#define SHADOW_FRAMEBUFFER_SIZE ((DISPLAY_WIDTH*DISPLAY_HEIGHT)/8)
+	static uint8_t shadow_framebuffer[SHADOW_FRAMEBUFFER_SIZE]; // Too big for heap
 
 	while (1)
 	{
@@ -76,14 +89,57 @@ int main(void)
 				break;
 		}
 
-		// Placeholder: Rainbow block
-		int y_block = y_root+1*(PILLAR_SIZE+PILLAR_GAP), x_block = x_root+1*(PILLAR_SIZE+PILLAR_GAP);
-		for(int y = 0; y < PILLAR_SIZE; y++) {
-			for(int x = 0; x < PILLAR_SIZE; x++) {
-				fb[at(x_block+x, y_block+y)] = color;
+		// At any one time we have two lists of points we can expand into;
+		// one for the current frame, and one for the next frame.
+		int next = (current+1)%2;
+		Candidate *candidates_current = &candidates[current][0];
+		Candidate *candidates_next = &candidates[next][0];
+
+		#define CANDIDATE_PUSH(candidate) { candidates_next[candidates_len[next]] = candidate; candidates_len[next]++; }
+
+		if (candidates_len[current] == 0) {
+			candidates_current[0] = make_candidate(DISPLAY_WIDTH/2, DISPLAY_HEIGHT/2);
+			candidates_len[current] = 1;
+		}
+
+		// Draw the current list (but only the lucky first handful)
+		for(int idx = 0; idx < WINNER_COUNT && idx < candidates_len[current]; idx++) {
+			Candidate winner = candidates_current[idx];
+			fb[AT(winner.x, winner.y)] = color;
+		}
+
+		// Prepare for next frame
+		// Since we don't have to worry about vblank finishing, we can take our time now.
+		for(int idx = 0; idx < SHADOW_FRAMEBUFFER_SIZE; idx++)
+			shadow_framebuffer[idx] = 0;
+		for(int idx = 0; idx < candidates_len[current] && candidates_len[next] < CANDIDATE_COUNT; idx++) {
+			Candidate check = candidates_current[idx];
+			Candidate neighbors[4] = {
+				{check.x, (check.y+1)%DISPLAY_HEIGHT},
+				{(check.x+1)%DISPLAY_WIDTH, check.y},
+				{check.x, (check.y+DISPLAY_HEIGHT-1)%DISPLAY_HEIGHT},
+				{(check.x+DISPLAY_WIDTH-1)%DISPLAY_WIDTH, check.y}
+			};
+			for(int n = 0; n < 4; n++) {
+				if (candidates_len[next] >= CANDIDATE_COUNT)
+					break;
+				size_t neighbor_at = AT(neighbors[n].x, neighbors[n].y);
+				// Use the shadow framebuffer to check for pixels we've checked this frame
+				size_t neighbor_shadow_idx = neighbor_at/8;
+				uint8_t neighbor_shadow_bit = 1<<(neighbor_at%8); 
+				if (shadow_framebuffer[neighbor_shadow_idx] & neighbor_shadow_bit)
+					continue;
+				shadow_framebuffer[neighbor_shadow_idx] |= neighbor_shadow_bit;
+				uint16_t color_minus_current = color-fb[neighbor_at];
+				int16_t color_minus_current_diff = color_minus_current;
+				if (color_minus_current_diff <= 0)
+					continue;
+				CANDIDATE_PUSH(neighbors[n]);
 			}
 		}
 
+		candidates_len[current] = 0;
+		current = next;
 		color++; // Treat the 565 bit-packed color as a single integer. Normally, you don't want this.
 	}
 
