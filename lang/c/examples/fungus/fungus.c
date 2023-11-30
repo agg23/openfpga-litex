8fa800220b279e3d2a101c740b27509a82398589
mcc <andi.m.mcclure@gmail.com>
2023-11-29 22:37:45 -0500

    Fungus: Pause with select, force reboot with start (even when not in +-to-reboot mode)

--- lang/c/examples/fungus/fungus.c
+++ lang/c/examples/fungus/fungus.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 
 // #include <irq.h>
@@ -21,6 +22,25 @@
 #define BITS6 ((1<<6)-1)
 // Takes 3 numbers in range 0..64. Lowest bit on R and B will be discarded.
 #define COLOR(r,g,b) ( ( (((r)>>1)&BITS5)<<11 ) | ( ((g)&BITS6)<<5 ) | ( (((b)>>1)&BITS5) ))
+
+typedef enum {
+    dpad_up     = 1<<0,
+    dpad_down   = 1<<1,
+    dpad_left   = 1<<2,
+    dpad_right  = 1<<3,
+    face_a      = 1<<4,
+    face_b      = 1<<5,
+    face_x      = 1<<6,
+    face_y      = 1<<7,
+    trig_l1     = 1<<8,
+    trig_r1     = 1<<9,
+    trig_l2     = 1<<10,
+    trig_r2     = 1<<11,
+    trig_l3     = 1<<12,
+    trig_r3     = 1<<13,
+    face_select = 1<<14,
+    face_start  = 1<<15,
+} PocketControls;
 
 // Sizes and spacing of "pillar" squares
 #define PILLAR_COUNT 3
@@ -120,6 +140,9 @@ int main(void)
 	uint16_t audio_wave = 1<<15;
 	uint16_t audio_wave_ceil = 0;
 
+	bool paused = false;
+	uint16_t cont1_key_last = 0;
+
 	while (1)
 	{
 		while (1) {
@@ -154,7 +177,9 @@ int main(void)
 		// but it did wind up sounding like a kind of bubbling cauldron, which I like.
 		size_t audio_needed = AUDIO_TARGET - apf_audio_buffer_fill_read();
 		for(size_t idx = 0; idx < audio_needed; idx++) {
-			if (0 == audio_cycle % AUDIO_GAP) {
+			if (paused) {
+				// Do nothing
+			} else if (0 == audio_cycle % AUDIO_GAP) {
 				if (audio_wave >= audio_wave_ceil) {
 					audio_cycle++;
 				} else {
@@ -185,41 +210,58 @@ int main(void)
 		}
 		apf_audio_playback_en_write(1);
 
+        uint16_t cont1_key = apf_input_cont1_key_read(); // Crop out analog sticks
+        uint16_t cont1_key_edge = (~cont1_key_last) & cont1_key;
+        cont1_key_last = cont1_key;
+
+        if (cont1_key_edge & face_select) {
+            paused = !paused;
+        }
+
+        if (cont1_key_edge & face_start) {
+            ctrl_reset_write(1); // 1 resets entire SOC
+        }
+
 		// Prepare for next frame
 		// Since we don't have to worry about vblank finishing, we can take our time now.
-		for(int idx = 0; idx < SHADOW_FRAMEBUFFER_SIZE; idx++)
-			shadow_framebuffer[idx] = 0;
-		for(int idx = 0; idx < candidates_len[current] && candidates_len[next] < CANDIDATE_COUNT; idx++) {
-			Candidate check = candidates_current[idx];
-			Candidate neighbors[4] = {
-				{check.x, (check.y+1)%DISPLAY_HEIGHT},
-				{(check.x+1)%DISPLAY_WIDTH, check.y},
-				{check.x, (check.y+DISPLAY_HEIGHT-1)%DISPLAY_HEIGHT},
-				{(check.x+DISPLAY_WIDTH-1)%DISPLAY_WIDTH, check.y}
-			};
-			for(int n = 0; n < 4; n++) {
-				if (candidates_len[next] >= CANDIDATE_COUNT)
-					break;
-				size_t neighbor_at = AT(neighbors[n].x, neighbors[n].y);
-				// Use the shadow framebuffer to check for pixels we've checked this frame
-				size_t neighbor_shadow_idx = neighbor_at/8;
-				uint8_t neighbor_shadow_bit = 1<<(neighbor_at%8); 
-				if (shadow_framebuffer[neighbor_shadow_idx] & neighbor_shadow_bit)
-					continue;
-				shadow_framebuffer[neighbor_shadow_idx] |= neighbor_shadow_bit;
-				uint16_t color_minus_current = color-fb[neighbor_at];
-				int16_t color_minus_current_diff = color_minus_current;
-				if (color_minus_current_diff <= 0)
-					continue;
-				CANDIDATE_PUSH(neighbors[n]);
+		if (!paused) { // Note we DON'T pause drawing, only updates and sound
+			for(int idx = 0; idx < SHADOW_FRAMEBUFFER_SIZE; idx++)
+				shadow_framebuffer[idx] = 0;
+			for(int idx = 0; idx < candidates_len[current] && candidates_len[next] < CANDIDATE_COUNT; idx++) {
+				Candidate check = candidates_current[idx];
+				Candidate neighbors[4] = {
+					{check.x, (check.y+1)%DISPLAY_HEIGHT},
+					{(check.x+1)%DISPLAY_WIDTH, check.y},
+					{check.x, (check.y+DISPLAY_HEIGHT-1)%DISPLAY_HEIGHT},
+					{(check.x+DISPLAY_WIDTH-1)%DISPLAY_WIDTH, check.y}
+				};
+				for(int n = 0; n < 4; n++) {
+					if (candidates_len[next] >= CANDIDATE_COUNT)
+						break;
+					size_t neighbor_at = AT(neighbors[n].x, neighbors[n].y);
+					// Use the shadow framebuffer to check for pixels we've checked this frame
+					size_t neighbor_shadow_idx = neighbor_at/8;
+					uint8_t neighbor_shadow_bit = 1<<(neighbor_at%8); 
+					if (shadow_framebuffer[neighbor_shadow_idx] & neighbor_shadow_bit)
+						continue;
+					shadow_framebuffer[neighbor_shadow_idx] |= neighbor_shadow_bit;
+					uint16_t color_minus_current = color-fb[neighbor_at];
+					int16_t color_minus_current_diff = color_minus_current;
+					if (color_minus_current_diff <= 0)
+						continue;
+					CANDIDATE_PUSH(neighbors[n]);
+				}
 			}
+
+			// Randomize candidates list so we don't just go continually downward
+			fisher_yates(candidates_next, candidates_len[next]);
+
+			candidates_len[current] = 0;
+			current = next;
+		} else {
+			// Also randomize candidates list while paused, for cool fuzz
+			fisher_yates(candidates_current, candidates_len[current]);
 		}
-
-		// Randomize candidates list so we don't just go continually downward
-		fisher_yates(candidates_next, candidates_len[next]);
-
-		candidates_len[current] = 0;
-		current = next;
 		color++; // Treat the 565 bit-packed color as a single integer. Normally, you don't want this.
 	}
 
